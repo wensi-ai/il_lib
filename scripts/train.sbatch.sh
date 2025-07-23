5a4bb503b5443959f13a1f934cf232b71a7b18b8
Wensi Ai <wsai@stanford.edu>
2025-07-22 22:10:45 -0700

    Update sbatch

--- scripts/train.sbatch.sh
+++ scripts/train.sbatch.sh
@@ -3,11 +3,11 @@
 #SBATCH --account=viscam
 #SBATCH --partition=viscam
 #SBATCH --exclude=viscam1
-#SBATCH --nodes=2
-#SBATCH --gres=gpu:a5000:4
-#SBATCH --ntasks-per-node=4
-#SBATCH --mem=240G
-#SBATCH --cpus-per-task=12
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a5000:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --mem=120G
+#SBATCH --cpus-per-task=16
 #SBATCH --time=1-00:00:00
 #SBATCH --output=outputs/sc/train_policy_%j.out
 #SBATCH --error=outputs/sc/train_policy_%j.err
@@ -20,13 +20,12 @@ echo "SLURM_JOBID="$SLURM_JOBID
 echo "SLURM_JOB_NAME="$SLURM_JOB_NAME
 echo "SLURM_JOB_NODELIST"=$SLURM_JOB_NODELIST
 echo "SLURM_NNODES"=$SLURM_NNODES
-echo "SLURM_GPUS"=$SLURM_GPUS
-echo "SLURMTMPDIR="$SLURMTMPDIR
+echo "SLURM_NTASKS"=$SLURM_NTASKS
 echo "working directory="$SLURM_SUBMIT_DIR
 
 source /vision/u/wsai/miniconda3/bin/activate behavior
 
-python train.py data_dir=/vision/u/wsai/behavior gpus=$SLURM_GPUS num_nodes=$SLURM_NNODES "$@"
+python train.py data_dir=/vision/u/wsai/behavior gpus=$SLURM_NTASKS num_nodes=$SLURM_NNODES "$@"
 
 echo "Job finished."
 exit 0
